a889c954d10384dd8c5c3f86cb0f59ccbf67fee7
ClaytonC <cjcanady1113@gmail.com>
2020-09-05 12:57:53 -0400

    upload to data base from front end

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home'
 import RecipeList from './RecipeList'
+import Timer from './Timer'
 
 import SingleRecipe from './SingleRecipe'
 function App() {
@@ -19,17 +20,20 @@ function App() {
 					<Link to='/recipe-list'>
 						<Button variant='dark'>Recipes</Button>
 					</Link>
+          <Link to='/timer'>
+						<Button variant='dark'>Timer</Button>
+          </Link>
 				</Nav>
 			</header>
 			<main>
 				<Route path='/' exact component={Home} />
+        <Route path='/timer' component={Timer}/>
 				<Route
 					path='/recipe-list'
 					render={() => {
 						return <RecipeList return />;
 					}}
 				/>
-
 				<Route
 					path='/recipe/:recipeID'
 					render={(routerProps) => {
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -3,10 +3,33 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 function RecipeList(props) {
 	const [data, setData] = useState([]);
+	const [name, setName] = useState([]);
+	const [description, setDescription] = useState('');
+	const [ingredients, setIngredients] = useState('');
+	const [instructions, setInstructions] = useState('');
+	const [photoUrl, setPhotoUrl] = useState('');
 	const fetchData = async () => {
 		const result = await axios('http://localhost:8000/recipes');
 		setData(result.data);
 	};
+		const addRecipe = (e) => {
+			
+			e.preventDefault();
+			axios
+				.post('http://localhost:8000/recipes', {
+					name: `${name}`,
+					description: `${description}`,
+					ingredients: `${ingredients}`,
+					instructions: `${instructions}`,
+					photo_url: `${photoUrl}`,
+				})
+				.then(fetchData());
+			setDescription('');
+			setIngredients('');
+			setInstructions('');
+			setPhotoUrl('');
+			setName('');
+		};
 	useEffect(() => {
 		fetchData();
 	}, []);
@@ -22,6 +45,39 @@ function RecipeList(props) {
 						<p>{recipe.description}</p>
 					</div>
 				))}
+			<form onSubmit={addRecipe}>
+				<label htmlFor='name'>name</label>
+				<input
+					type='text'
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<label htmlFor='description'>description</label>
+				<input
+					type='text'
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
+				/>
+				<label htmlFor='ingredients'>ingredients</label>
+				<input
+					type='text'
+					value={ingredients}
+					onChange={(e) => setIngredients(e.target.value)}
+				/>
+				<label htmlFor='instructions'>instructions</label>
+				<input
+					type='text'
+					value={instructions}
+					onChange={(e) => setInstructions(e.target.value)}
+				/>
+				<label htmlFor='photoUrl'>photo url</label>
+				<input
+					type='text'
+					value={photoUrl}
+					onChange={(e) => setPhotoUrl(e.target.value)}
+				/>
+				<input type='submit' />
+			</form>
 		</div>
 	);
 }
